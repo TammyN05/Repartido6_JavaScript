9ce4af737ddf203120684b9d1d4c57f4f948644e
braian9 <braianmartinez123456@gmail.com>
2022-05-04 18:06:03 -0300

    Tam

--- Repartido7.js
+++ Repartido7.js
@@ -2,20 +2,20 @@
 
 // 6 - Números pares entre 0 y un máximo
 
-function contadorDePares (min , max){
+/*function contadorDePares (min , max){
     for (let i = min; i <= max; i++)
         if (i % 2 === 0){
             console.log (i)
         }
 }
 
-contadorDePares (10 , 20)
+contadorDePares (10 , 20)*/
 
 /*el for se usa para los loop*/
 
 //FORMA DI EL USUARIO INGRESA LOS NUMEROS
 
-function contadorDePares (){
+/*function contadorDePares (){
     let min = parseInt (prompt("ingrese un minimo"))
     let max = parseInt (prompt("ingrese un maximo"))
     for (let i = min; i <= max; i++)
@@ -23,10 +23,10 @@ function contadorDePares (){
             console.log (i)
         }
 }
-contadorDePares ()
+contadorDePares ()*/
 
 // OTRA FORMA
-function contadorDePares (max){
+/*function contadorDePares (max){
     let contador = 0;
     for (let i = 0; i<= max; i++){
         if(i % 2 === 0){
@@ -35,5 +35,85 @@ function contadorDePares (max){
     }
     return contador
 }
- console.log (contadorDePares(30))
+ console.log (contadorDePares(30))*/
+
+
+// 7 - Mayor(edad, tope): Escribir la función Mayor  que reciba una edad 
+// y un valor tope y retorne true si la edad supera a ese tope. 
+//En caso contrario que retorne false.
+
+
+/*let edad = parseInt(prompt("Ingrese una edad"))
+let tope = 18
+
+function Mayor (edad, tope){
+    if (edad >= tope){
+        alert("ok, eres mayor")
+    }else {
+        alert("tas chikito")
+    } 
+    return (Mayor)
+}
+Mayor (edad, tope)*/
+
+
+/* 8 - Usuario y contraseña: 
+Solicitar al usuario su nombre de usuario y contraseña para ingresar al sistema mediante prompts. 
+Crear una función para validar que las credenciales ingresadas son válidas. 
+(definir las credenciales válidas en constantes).*/
+
+
+/*prompt("Ingrese el usuario")
+prompt("Ingrese la contraseña")
+
+let user = "Tamara"
+let password = "provi"
+
+function login (user, password){
+    if (user == "Tamara" && password == "provi"){
+        alert("Welcome");
+    } else {
+        alert ("OW NO OW NO OW NONOO");
+    }
+    return (login);
+}
+login(user, password);*/
+
+/* 9 - Pseudocódigo a javascript: Escribir el siguiente programa en pseudocódigo a código javaScript 
+ Hacer 
+    n1 = Ingrese un numero
+    n2 = Ingrese otro numero 
+Si (n1 y n2 son numero enteros)
+    mostrar suma de n1 y n2
+No
+    Mensaje de error
+
+Repetir mientras suma sea menor a 100*/
+
+/*let n1 = parseFloat(prompt("Ingrese un numero"));
+let n2 = parseFloat(prompt("Ingrese otro numero"));
+
+function Suma(x,y){
+    if ((x + y) != parseInt(x + y)){
+        alert ("ERROR");
+    } else {
+        alert("La suma es: "+ (parseInt(x) + parseInt(y)))
+    }
+ return (Suma)
+}
+Suma(n1, n2)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
